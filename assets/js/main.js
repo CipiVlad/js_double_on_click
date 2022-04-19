359a0bde0996b118551275c197687342631057a9
CipiVlad <supercoder.cipi@gmail.com>
2022-04-19 15:53:28 +0200

    doubleonclick

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let inputNumber = document.getElementById('number');
+let output = document.getElementById('form-output');
+
+function double() {
+    // console.log(inputNumber.value * 2);
+    output.innerHTML = inputNumber.value * 2;
+};
+
